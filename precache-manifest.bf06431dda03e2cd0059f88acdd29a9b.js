c6068d2be23d91fa302984c79d6b042cdadd167e
ad-ahanadutta <ahanadutta555@gmail.com>
2020-10-14 06:20:25 +0530

    Updates

---
+++ precache-manifest.bf06431dda03e2cd0059f88acdd29a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d20d815aeba1d32324bfb721dccdfae",
+    "url": "/Bookoholic/index.html"
+  },
+  {
+    "revision": "650bf8372ef98db3c62a",
+    "url": "/Bookoholic/static/css/main.9b7cd8de.chunk.css"
+  },
+  {
+    "revision": "185b4a4fdf3887ad3735",
+    "url": "/Bookoholic/static/js/2.bee72a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bookoholic/static/js/2.bee72a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650bf8372ef98db3c62a",
+    "url": "/Bookoholic/static/js/main.97f263c0.chunk.js"
+  },
+  {
+    "revision": "4f407004ca1586082093",
+    "url": "/Bookoholic/static/js/runtime-main.3138a426.js"
+  }
+]);
